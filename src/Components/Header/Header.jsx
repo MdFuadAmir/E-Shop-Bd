4a70e87aab9aae9f936dc5ffb9802cdb238629e3
Md Fuad Amir <mdfuadamir@gmail.com>
2024-05-01 22:11:06 +0600

    added header section complete

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,10 +1,75 @@
+import { IoMdSearch } from "react-icons/io";
+import { FaCartShopping } from "react-icons/fa6";
+import DarkMode from "./DarkMode";
 
+const menueList = [
+    {
+        id: 1,
+        name: "Home",
+        link: "/#"
+    },
+    {
+        id: 2,
+        name: "Shop",
+        link: "/shop"
+    },
+    {
+        id: 3,
+        name: "About",
+        link: "/about"
+    },
+    {
+        id: 4,
+        name: "Blogs",
+        link: "/blogs"
+    },
+]
 
 const Header = () => {
     return (
-        <div>
-            
+        <>
+        <div className="bg-white dark:bg-gray-900 dark:text-white duration-200 relative z-40">
+            <div className="py-4">
+                <div className="container flex items-center justify-between">
+                    {/* logo and links sections  */}
+                    <div className="flex items-center gap-4">
+                        <a href="#" className="text-primary font-semibold text-2xl tracking-wide uppercase sm:text-3xl">E-Shop</a>
+                        {/* menu items */}
+                        <div className="hidden lg:block">
+                            <ul className="flex gap-4 item-center">
+                                {
+                                    menueList.map((data,index) =>(
+                                        <li key={index}><a className="inline-block px-4 font-semibold text-gray-500 hover:text-black dark:hover:text-white" href={data.link}>{data.name}</a></li>
+                                    ))
+                                }
+                            </ul>
+                        </div>
+                    </div>
+                    {/* Navber Right  sections  */}
+                    <div className="flex justify-between items-center gap-4">
+                        {/* search bar sevction */}
+                        <div className="relative group hidden sm:block">
+                            <input type="text" placeholder="Search" className="search-bar" /><IoMdSearch className="text-xl text-gray-600 dark:text-gray-400 absolute top-1/2 -translate-y-1/2 right-3 group-hover:text-primary duration-200"/>
+                        </div>
+                        {/* order btn */}
+                        <div>
+                            <button className="relative p-3">
+                                <FaCartShopping className="text-xl text-gray-600 dark:text-gray-400"/>
+                                <div className="w-4 h-4 bg-red-500 text-white rounded-full absolute top-0 right-0 flex items-center justify-center text-xs">
+                                    4
+                                </div>
+                            </button>
+                        </div>
+                        {/* dark mode section */}
+                        <div>
+                            <DarkMode></DarkMode>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
+        </>
+    
     );
 };
 
