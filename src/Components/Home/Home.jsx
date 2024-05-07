7e9e73d1d5cb9c342309217b41479506b0468003
Md Fuad Amir <mdfuadamir@gmail.com>
2024-05-07 16:39:21 +0600

    added banner section complete

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,9 +1,21 @@
+import Banner from "../Banner/Banner";
 import Category from "../Category/Category";
 import Category2 from "../Category/Category2";
 import Header from "../Header/Header";
 import Hero from "../Hero/Hero";
 import Services from "../Services/Services";
+import headphone from "../../assets/hero/headphone.png"
 
+const bannerData ={
+    discount: "30% OFF",
+    title: "Fine Smile",
+    date: "7 may to 15 may",
+    image: headphone,
+    title2: "Air Solo Bass",
+    title3: "Winter Sale",
+    title4: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sapiente amet necessitatibus numquam minus dicta placeat quae minima cupiditate.",
+    bgColor: "#f42c37",
+}
 
 const Home = () => {
     return (
@@ -13,7 +25,7 @@ const Home = () => {
             <Category />
             <Category2 />
             <Services/>
-            
+            <Banner data={bannerData}/>
         </div>
     );
 };
